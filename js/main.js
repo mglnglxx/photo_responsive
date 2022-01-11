6953387ec9751e025d0b6ca94b5e01fd57ac643f
miguel rodriguez <aphex95@gmail.com>
2022-01-11 18:36:17 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Animate Smooth Scrolling
+$("#view-work").on("click", function () {
+  const images = $("#images").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images,
+    },
+    900
+  );
+});
